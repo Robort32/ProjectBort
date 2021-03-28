22cbe18bb6aa81e5ff081a37a2e91f2444e57ed9
Holly Jasiura <hollyj@Im-a-Computer.local>
2021-03-27 21:37:35 -0400

    JS is populating! :boom:

--- scripts/script.js
+++ scripts/script.js
@@ -44,34 +44,33 @@ projectBort.optionsArray = [
 ];
 
 projectBort.api = "https://api.boardgameatlas.com/api/search?";
-
-projectBort.clientID = "pKTceFALuw";
+projectBort.clientID = "b8a4fHq3xL";
 
 //DISABLING BUTTON UNTIL OPTIONS ARE FILLED
 
 projectBort.theMasterFunction = () => {
   //submit button
   projectBort.submitButton = document.querySelector(".submitBtn");
-  //Value of our options drop down
-  projectBort.mechanicsOption = document.querySelector("#optionSelect").value;
-  //getting the player option
-  projectBort.playerOption = document.querySelector("#playerOption").value;
 
   projectBort.submitButton.addEventListener("click", function (e) {
     e.preventDefault();
-    // projectBort.buttonDisable();
-    console.log("CLICKS");
 
-    //chainge the min player dropdown to a number
-    let playerOptionNumber = parseInt(projectBort.playerOption, 10);
+    //Value of our options drop down
+    projectBort.mechanicsOption = document.querySelector("#optionSelect").value;
+
+    //getting the player option
+    projectBort.playerOption = document.querySelector("#playerOption").value;
 
     //iteraiting though the created array full of objects to see which option was selected and retruning the correct mechId
     projectBort.optionsArray.forEach((e) => {
       if (e.value === projectBort.mechanicsOption) {
-        //project.bort.useInMech gets passed the meachanics params
         projectBort.useInMech = e.mechId;
       }
     });
+
+    //chainge the min player to a number
+    const playerOptionNumber = parseInt(projectBort.playerOption, 10);
+
     //Getting Our API
     const url = new URL(projectBort.api);
     url.search = new URLSearchParams({
@@ -87,17 +86,40 @@ projectBort.theMasterFunction = () => {
       })
       //THIS IS WHERE WE GET THE DATAS
       .then((jsonResponse) => {
-        console.log(jsonResponse);
-        //This forEach was me going thought checking to make sure Each Game that came up Actually had the ID on it
-
-        // jsonResponse.games.forEach(function (e) {
-        //   console.log(e.mechanics[0].id);
-
-        // });
+        console.log("API RETURNS");
+        projectBort.showGames(jsonResponse);
       });
   });
-  //});
 };
+
+projectBort.showGames = (result) => {
+  const gameResultContainer = document.querySelector(".gameResultContainer");
+  const resultArray = result.games;
+
+  const gameContent = resultArray.map((x) => {
+    return `<div class="gameCard">
+      <div class="gameImageContainer">
+        <img
+          src=${x.image_url}
+        alt=""
+        id="gameImage"
+      />
+    </div>
+  
+    <div class="gameTextContainer">
+      <h2 id="gameTitle">${x.name}</h2>
+      <div class="lineBreak"></div>
+      <div class="gameDetail">
+        <p id="gameCategory">CATEGORY</p>
+      </div>
+      <div class="gameDetail">
+        <p id="gamePrice">20.00</p>
+      </div>
+    </div></div>`;
+  });
+  gameResultContainer.innerHTML = gameContent.join("");
+};
+
 projectBort.init = () => {
   projectBort.theMasterFunction();
 };
