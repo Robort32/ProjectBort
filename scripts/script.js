ca34a64554df484afba5a6370a80beb89fccaab1
Holly Jasiura <hollyj@Im-a-Computer.local>
2021-03-31 21:17:26 -0400

    GOT THAT SEARCH, BABY@@@

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,22 @@
 const projectBort = {};
-//For the game mechanic drop down.
-projectBort.optionsArray = [
-  {
-    value: "Betting",
-    mechId: "3tuJiW3pps",
-  },
-  {
-    value: "Campaign",
-    mechId: "xuphiSlrxI",
-  },
-  {
-    value: "Action",
-    mechId: "Bc7R8pLoGk",
-  },
-  {
-    value: "Network and Route Building",
-    mechId: "ohABM4GjbC",
-  },
-  {
-    value: "Player Elimination",
-    mechId: "BGrhzIN69D",
-  },
-  {
-    value: "Race",
-    mechId: "qZx4PEzKKz",
-  },
-  {
-    value: "Trading",
-    mechId: "AVY6EvSQTP",
-  },
-  {
-    value: "Cooperative play",
-    mechId: "9mNukNBxfZ",
-  },
-  {
-    value: "Acting",
-    mechId: "n1GtBt35Rd",
-  },
-  {
-    value: "Deduction",
-    mechId: "GsNGxZFNCK",
-  },
-];
-
-projectBort.api = "https://api.boardgameatlas.com/api/search?";
 
 projectBort.clientID = "b8a4fHq3xL";
 
 projectBort.submitDataToApi = () => {
   projectBort.submitBtn = document.querySelector(".submitBtn");
+
   projectBort.submitBtn.addEventListener("click", function (e) {
     e.preventDefault();
+
+    //grab the value of Mechanics and Categories (loaded in window from API first thing)
     projectBort.useMerchanics = document.querySelector("#mechOption").value;
     projectBort.useCategories = document.querySelector("#categoryOption").value;
 
-    // projectBort.mechanics();
+    //creating the price window selected by user
     projectBort.pricePoint();
+    //creating the window of min/max players as selected by user
     projectBort.minMaxPlayers();
+    //sending all the collected data to be called to API
     projectBort.apiCall(
       projectBort.minPlayerNumber,
       projectBort.maxPlayerNumber,
@@ -67,6 +27,7 @@ projectBort.submitDataToApi = () => {
     );
   });
 };
+//
 //Getting Value for minimum players and maximum players
 projectBort.minMaxPlayers = () => {
   projectBort.minPlayerOption = document.querySelector(
@@ -78,18 +39,11 @@ projectBort.minMaxPlayers = () => {
   projectBort.minPlayerNumber = parseInt(projectBort.minPlayerOption, 10);
   projectBort.maxPlayerNumber = parseInt(projectBort.maxPlayerOption, 10);
 };
-//getting value for mechanics
-// projectBort.mechanics = () => {
-//   projectBort.mechanicsOption = document.querySelector("#mechOption").value;
-//   projectBort.optionsArray.forEach((e) => {
-//     if (e.value === projectBort.mechanicsOption) {
-//       projectBort.useInMech = e.mechId;
-//     }
-//   });
-// };
+//
 //get value for price
 projectBort.pricePoint = () => {
   projectBort.priceOption = document.querySelector("#priceOption").value;
+
   let priceNumber = parseInt(projectBort.priceOption, 10);
   if (priceNumber === 75) {
     projectBort.priceGreaterThen = 75;
@@ -99,6 +53,7 @@ projectBort.pricePoint = () => {
     projectBort.priceLowerThen = priceNumber + 25;
   }
 };
+//
 //hide robort section when games are received OR display "nothing found"
 projectBort.hideRobortSection = (info) => {
   projectBort.robortSection = document.querySelector(".robortSection");
@@ -115,7 +70,8 @@ projectBort.hideRobortSection = (info) => {
   }
 };
 
-//Getting Our API
+//
+//Getting the information from the API
 projectBort.apiCall = (
   minPlayers,
   maxPlayers,
@@ -125,20 +81,73 @@ projectBort.apiCall = (
   categories
 ) => {
   //////
-
-  //////
+  projectBort.api = "https://api.boardgameatlas.com/api/search?";
   const url = new URL(projectBort.api);
-  url.search = new URLSearchParams({
+  // const parms = new URLSearchParams(url.search);
+  const searchParams = {
     client_id: projectBort.clientID,
-    limit: 20,
+    // limit: 20,
     min_players: minPlayers,
     mechanics: mechanics,
     gt_price: gtprice,
     lt_price: ltprice,
     gt_max_players: maxPlayers,
     categories: categories,
+  };
+
+  const cleanUrl = new URLSearchParams();
+  Object.entries(searchParams).forEach((value) => {
+    if (
+      value[1] !== "makeASelection" &&
+      !(typeof value[1] === "number" && isNaN(value[1]))
+    ) {
+      cleanUrl.set(value[0], value[1]);
+    }
   });
-  console.log(url);
+  url.search = cleanUrl;
+
+  // const keysToDelete = [];
+  // searchParams.forEach((v, k) => {
+  //   // console.log(k, v);
+  //   if (v === "makeASelection") {
+  //     // console.log(k, v);
+  //     keysToDelete.push(k);
+  //   }
+  //   // console.log(keysToDelete)
+  // });
+  // console.log(keysToDelete);
+  // serachParams.forEach((v, k) => {
+  //   console.log(v, k);
+  // });
+
+  // parms.set("client_id", projectBort.clientID);
+
+  // if (categories !== "makeASelection") {
+  //   parms.set("categories", categories);
+  // }
+  // if (minPlayers !== "makeASelection") {
+  //   parms.set("min_player", minPlayers);
+  // }
+
+  // parms.set("categories", categories);
+
+  //////
+  /////
+
+  // projectBort.api = "https://api.boardgameatlas.com/api/search?";
+  // const url = new URL(projectBort.api);
+  // url.search = new URLSearchParams({
+  //   client_id: projectBort.clientID,
+  //   limit: 20,
+  //   min_players: minPlayers,
+  //   mechanics: mechanics,
+  //   gt_price: gtprice,
+  //   lt_price: ltprice,
+  //   gt_max_players: maxPlayers,
+  //   categories: categories,
+  // });
+  // console.log(url);
+
   fetch(url)
     .then((res) => {
       return res.json();
@@ -148,8 +157,8 @@ projectBort.apiCall = (
       projectBort.hideRobortSection(jsonResponse);
     });
 };
-
-//show game results
+//
+//show game results in cards
 projectBort.showGames = (result) => {
   const resultArray = result.games;
   //check to make sure templates are supported (catch added to fetch statement)
@@ -177,7 +186,57 @@ projectBort.showGames = (result) => {
     console.log("Your browser does not support templates");
   }
 };
-
+//
+//things that run on the page load - populating drop downs & general stylings
+projectBort.pageLoad = () => {
+  window.addEventListener("load", () => {
+    projectBort.loadDropdowMechanic();
+    projectBort.loadDropdowCategorgies();
+    projectBort.returnToTop();
+  });
+};
+//
+//API call to populate game mechanic dropdown
+projectBort.loadDropdowMechanic = () => {
+  const mechanicUrl = new URL(
+    "https://api.boardgameatlas.com/api/game/mechanics?"
+  );
+  mechanicUrl.search = new URLSearchParams({
+    client_id: projectBort.clientID,
+  });
+  fetch(mechanicUrl).then((res) => {
+    res.json().then((response) => {
+      projectBort.populateDropdown(response.mechanics, "#mechOption");
+    });
+  });
+};
+//
+//API call to populate game category dropdown
+projectBort.loadDropdowCategorgies = () => {
+  const categoryUrl = new URL(
+    "https://api.boardgameatlas.com/api/game/categories?"
+  );
+  categoryUrl.search = new URLSearchParams({
+    client_id: projectBort.clientID,
+  });
+  fetch(categoryUrl).then((res) => {
+    res.json().then((response) => {
+      projectBort.populateDropdown(response.categories, "#categoryOption");
+    });
+  });
+};
+//
+//shared function to populate the dropdowns from a window load API call (categories & mechanics)
+projectBort.populateDropdown = (apiResult, location) => {
+  const dropdownLocation = document.querySelector(location);
+  apiResult.forEach((item) => {
+    const gameOption = document.createElement("option");
+    gameOption.textContent = item.name;
+    gameOption.value = item.id;
+    dropdownLocation.appendChild(gameOption);
+  });
+};
+//
 //Hiding/unhiding the back to top button
 projectBort.returnToTop = () => {
   const backToTop = document.getElementById("returnToTop");
@@ -194,53 +253,12 @@ projectBort.returnToTop = () => {
     }
   });
 };
-
-projectBort.loadDropdowMechanic = () => {
-  projectBort.apiMechanics =
-    "https://api.boardgameatlas.com/api/game/mechanics?";
-  window.addEventListener("load", () => {
-    const url = new URL(projectBort.apiMechanics);
-    url.search = new URLSearchParams({
-      client_id: projectBort.clientID,
-    });
-    fetch(url).then((res) => {
-      res.json().then((response) => {
-        projectBort.populateDropdown(response.mechanics, "#mechOption");
-      });
-    });
-  });
-};
-projectBort.loadDropdowCategorgies = () => {
-  projectBort.apiCategories =
-    "https://api.boardgameatlas.com/api/game/categories?";
-  window.addEventListener("load", () => {
-    const url = new URL(projectBort.apiCategories);
-    url.search = new URLSearchParams({
-      client_id: projectBort.clientID,
-    });
-    fetch(url).then((res) => {
-      res.json().then((response) => {
-        projectBort.populateDropdown(response.categories, "#categoryOption");
-      });
-    });
-  });
-};
-//shared function to populate the dropdowns from a window load API call (categories & mechanics)
-projectBort.populateDropdown = (apiResult, location) => {
-  const dropdownLocation = document.querySelector(location);
-  apiResult.forEach((item) => {
-    const gameOption = document.createElement("option");
-    gameOption.textContent = item.name;
-    gameOption.value = item.id;
-    dropdownLocation.appendChild(gameOption);
-  });
-};
-
+//
+//
+//go get it!
 projectBort.init = () => {
-  projectBort.loadDropdowMechanic();
-  projectBort.loadDropdowCategorgies();
+  projectBort.pageLoad();
   projectBort.submitDataToApi();
-  projectBort.returnToTop();
 };
 
 projectBort.init();
